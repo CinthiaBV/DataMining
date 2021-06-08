d955f991ed59e758f077471780567934305cfd4e
CinthiaBV <cinthia.vargas18@tectijuana.edu.mx>
2021-06-08 13:02:58 -0700

    Uploading third part of the Test3

--- Unit-3/Test/Test3.R
+++ Unit-3/Test/Test3.R
@@ -34,3 +34,24 @@ y_pred
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
 cm
+
+# Visualising the Training set results, first import the library ElemStatLearn 
+library(ElemStatLearn)
+
+# In this part we are using the training set to a new graph to visualize the training set results
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+
+# Give a name at the columns to use and predict also set the limit of the plot and other settings
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = ' Naive Bayes  Classifier (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
