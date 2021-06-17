deb76e35f06ceb2f1d708f5135be2cba3039b59b
Francisco Diaz <60798570+xChronos11@users.noreply.github.com>
2021-06-17 12:50:23 -0700

    Second part of test 4

--- Unit-4/Test4.R
+++ Unit-4/Test4.R
@@ -15,3 +15,19 @@ dataset = read.csv ('iris.csv')
 # Delete the species column and scale the data since we only need numerical values
 dataset.scale <- scale (dataset [, -5])
 
+#The previous count of each type of species is carried out
+dataset $ species
+
+# Application of K MEANS this receives two parameters: data and number of clusters to form.
+datameans = kmeans (dataset.scale, 3)
+datameans
+
+# Get group to which the data belongs is obtained according to the kmeans function.
+datameans $ cluster
+table (datameans $ cluster, dataset $ species)
+
+# We assign the class variable, the numerical data as a factor of the data set.
+class = factor (dataset [, 5]) ## classifies and stores data in levels
+
+# We assign to the variable predicts, the classes predicted by kmeans.
+predicts = NULL
